45b54ca0db10a8a439500caeb5e4cc24f71876eb
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-06-17 14:25:10 -0700

    h

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/**
+ *main -prints exactly the statement, followed by the new line
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
