b6b9fff7281bda78f63b1797155d412201e43b52
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-06-25 13:55:11 -0700

    more nested loops, print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print line - print lines
+ * @n: first parameter
+ * Description: '_' is input dependent
+ * Return: Returns nothing since function is void
+ */
+
+void print_line(int n)
+{
+int iteration;
+{
+for (iteration = 1; iteration <= n; iteration++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
+  
