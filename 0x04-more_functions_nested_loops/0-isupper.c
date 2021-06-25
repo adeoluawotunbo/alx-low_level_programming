c27d64e3b229b5452f5cc2701e0a7192576548a6
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-06-25 07:30:29 -0700

    nestedloops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _isupper - Check if parameter c represents an uppercase letter.
@@ -9,9 +8,10 @@
 
 int -isupper(int c)
 {
-if (c >= 65 && c <= 90)
+if (c >= 'A' && c <= 'Z')
 {
 return (1);
 }
+ else
 return (0);
 }
