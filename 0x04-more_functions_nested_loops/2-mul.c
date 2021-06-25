8abeb639169cb1cb606af0e3ba1545bc3e7b9a63
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-06-25 12:58:45 -0700

    more nested loops - multiplyimg two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Multiplies 2 integers
+ * @a: the first integer value to be multiplied
+ * @b: the second integer value to be multiplied
+ * Return: the value gotten from multiplying a and b together
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
