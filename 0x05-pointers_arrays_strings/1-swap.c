4e9b7cbdbdab469851c492ddb02a04911469b2a1
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-06-29 12:19:57 -0700

    switch integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *swap_int - swaps the values of two integers
+ * @a: First pointer
+ * @b: the second pointer
+ * Return: return nothing, a void function
+ */
+
+void swap_int(int *a, int *b)
+{
+int temp;
+int array[];
+temp = *a;
+*a = *b;
+*b = temp;
+}
