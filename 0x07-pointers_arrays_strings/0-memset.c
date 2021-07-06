81148cbace43398d7b6e948cd8a583ef7547af35
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-07-06 08:09:02 -0700

    pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,24 +2,17 @@
 
 /**
  * *_memset - This is the main function
- * @s: pointer to the buffer
+ * @s: pointer to the memory area too be filled
  * @b: The constant byte
- * @n: size in bytes of buffer to be filled
+ * @n: the number of bytes to be filled
  *
  * Description: This function fills memory area
  * with a constant byte
  *
- * Return: A pointer to memory area s.
+ * Return: A pointerto the filled memory @s.
  */
 
 char *_memset(char *s, char b, unsigned int n);
 {
-unsigned int i = 0;
-
-while (i < n)
-{
-s[i] = b;
-i++;
-}
 return (s);
 }
