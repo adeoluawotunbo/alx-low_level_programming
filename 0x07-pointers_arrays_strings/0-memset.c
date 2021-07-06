d25a9bfa5062c182ac0690fa9a93555cac547424
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-07-06 08:17:59 -0700

    pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,7 +12,16 @@
  * Return: A pointerto the filled memory @s.
  */
 
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
+  
 {
+unsigned int i = 0;
+
+while (i < n)
+{
+s[i] = b;
+i++;
+}
+    
 return (s);
 }
