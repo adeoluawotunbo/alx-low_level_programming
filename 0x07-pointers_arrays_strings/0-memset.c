3ea4c4884a612fa1c9a281886dfc75a1207cd761
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-07-06 08:50:37 -0700

    pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 
 /**
- * *_memset - This is the main function
- * @s: pointer to the memory area too be filled
- * @b: The constant byte
- * @n: the number of bytes to be filled
- * Return: A pointer to the filled memory @s.
+ * _memset.c - this is the main function
+ *@s - this is the pointer to the area to be filled
+ *@b - this fills the memoey area
+ *@n - this is number of bytes that will be filled in the
+ *memory area pointed to @s
  */
+
 
 char *_memset(char *s, char b, unsigned int n)
   
