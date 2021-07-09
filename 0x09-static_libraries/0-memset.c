ba226468a3809210d71b9b8ed459878a4297ec66
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-09 13:49:32 +0100

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -6,15 +6,16 @@
  * @b: the value to replace the n characters
  * @n: the number of characters to replace
  *
- * Description: n chars are replaced
+ * Description:n chars are replaced
  * Return: Always(0) Success
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 unsigned int i;
+char *index = (char *)s;
 
 for (i = 0; i < n; i++)
-s[i] = b;
+index[i] = b;
 
-return (s);
+return (index);
 }
