97cb14ff677f637cbc760d8e8f9b46fd68242750
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-09 13:25:29 +0100

    static libraries

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * _isupper - returns 1 or 0 based on input
+ * @c: first parameter
+ *
+ * Description: checks if a character is uppercase
+ * Return: Always(0) Success
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
