ca7f322367bd82fe1a48eb5dc2759f3acad77ae2
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-12 12:35:06 +0100

    number of files after the executable file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of files followed by a new line
+ * @argc: Number of arguement
+ * @argv: Array of the arguements
+ *
+ * Description: prints number of files followed by a new line
+ * Return: Always (0) for sucess
+ */
+
+int main(int argc, char *argv[])
+
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
