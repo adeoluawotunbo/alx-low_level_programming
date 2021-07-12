536d7079924a3522278f08cc9e40d81725b43ebc
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-12 13:59:45 +0100

    run

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: prints all arguement it receives;
+ * printing only one arguement per line.
+ * Return: Always(0) Success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i-1]);
+i++;
+}
+return (0);
+}
