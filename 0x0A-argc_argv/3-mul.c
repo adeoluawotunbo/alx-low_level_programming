87f42ad17ba5c61b47e7639a8b90f968e6394742
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-12 14:36:36 +0100

    g

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: arguement count
+ * @argv: arguement array
+ *
+ * Description: prints the result of the
+ * multiplication, followed by a new line
+ * Return: Always(0) Success
+ */
+int main(int argc, char *argv[])
+{
+int mul;
+int i;
+int argv[i] = a;
+int argv[i+1] = b;
+
+for (i = 1; i < argc)
+for (mul = a * b)
+{
+printf("%d\n", (mul));
+}
+	return (0);
+}
