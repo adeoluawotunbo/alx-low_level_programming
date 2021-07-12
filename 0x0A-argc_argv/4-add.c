d588fb164e611fa332b283dcabedd745481b108e
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-12 17:41:24 +0100

    j

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,21 +9,23 @@ int main(int argc, char *argv[])
 {
 int i, sum = 0;
  
-if (argc = 0)
+
+if (argc == 1)
 {
 printf("%d\n", 0);
 }
 
-if (!isdigit(argv[i]))
+for (i = 1; i < argc; i++);
+
+if (atoi(argv[i]) == 0)
 {
 printf("%s\n", "Error");
 return (1);
 }
 
-for (i = 1; i < argc; i++);
-{
 sum += atoi(argv[i]);
-}
+{
 printf("%d\n", sum);
 return (0);
+}
 }
