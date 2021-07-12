96613f03c298f63f80524d4645542807a172d864
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-12 14:58:35 +0100

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,21 +6,33 @@
  * @argv: arguement array
  *
  * Description: prints the result of the
- * multiplication, followed by a new line
- * Return: Always(0) Success
+ * multiplication of two numbers, followed 
+ * by a new line
+ * Return: If the program does not
+ * receive two arguments, return 1 otherwise 0
  */
+
 int main(int argc, char *argv[])
 {
-int mul;
 int i;
-argv[i] = int a;
-argv[i+1] = int b;
+int num1, num2, mul;
+char error = "Error";
 
-i = 1; 
-i < argc;
-mul = a * b;
+if (argc != 3)
 {
-printf("%d\n", (mul));
+printf("%s\n", error);
+return (1);
 }
-	return (0);
+
+while (i < argc)
+{
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+i++;
 }
+mul = num1 * num2;
+printf("%d\n", mul);
+
+return (0);
+}
+
