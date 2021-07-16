82a0f772bb4c32347a6b0497e935d8ad388826ca
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-07-16 06:48:24 -0700

    k

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,22 +1,3 @@
-#include <stdlib.h>
-#include "holberton.h"
-
-/**
- * _strlen - finds the length of a string
- * @s: string.
- * Return: String.
- */
-
-int _strlen(char *s)
-
-{
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
-		;
-return (i);
-}
-
 /**
  * string_nconcat - concatenate two strings.
  * @s1: first string.
@@ -29,33 +10,14 @@ return (i);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-unsigned int i, a, b;
-char *c;
+unsigned int s1_len, s2_len, i;
+char *str;
 
 if (s1 == NULL)
-{
 s1 = "";
-}
+
 if (s2 == NULL)
-{
 s2 = "";
-}
 
-a = _strlen(s1);
-b = _strlen(s2);
-
-if (n >= b)
-n = b;
-
-c = malloc(sizeof(char) * (a + n + 1));
-if (c == NULL)
-return (NULL);
-
-for (i = 0; i < a; i++)
-c[i] = s1[i];
-for (; i < a + n; i++)
-c[i] = s2[i - a];
-c[a + n] = '\0';
-
-return (c);
+return (str);
 }
