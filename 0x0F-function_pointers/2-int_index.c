c6ef0fdf75af31a8330490e98b7ce6b35c77081c
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-21 16:56:41 +0100

    k

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,18 +16,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (size <= 0 || array == NULL || cmp == NULL)
-{
-return (-1);
-}
-
 if (size > 0 && cmp && size && array)
-for (i = 0; i < size; i++)
-{
+for (i = 0; i <= size; i++)
+
 if (cmp(array[i]))
-{
 return (i);
-}
-}
+
 return (-1);
 }
