ce6f70bd2f21721519022ef555766cd46756a9ee
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-21 21:24:12 +0100

    push

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,8 +13,9 @@
  */
 int main(int argc, char *argv[])
 {
+
 int num1, num2;
-char *oprt
+char *op;
 
 if (argc != 4)
 {
@@ -23,25 +24,22 @@ exit(98);
 }
 
 num1 = atoi(argv[1]);
-oprt = argv[2];
+op = argv[2];
 num2 = atoi(argv[3]);
 
-if (get_op_func(oprt) == NULL || oprt[0] != '\0')
+if (get_op_func(op) == NULL || op[0] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
 
-if ((*oprt == '/' && num2 == 0) ||
-(*oprt == '%' && num2 == 0))
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
 
-printf("%d\n", get_op_func(oprt)(num1, num2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
-
-
-
