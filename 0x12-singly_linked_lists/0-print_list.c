2e787d248290a71b3ee8eddda57d5bed3b226821
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-29 20:39:56 +0100

    linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,6 @@
  */
 
 size_t print_list(const list_t *h)
-
 {
 size_t number_Of_Node = 0;
 const list_t *temp = h;
@@ -19,10 +18,17 @@ const list_t *temp = h;
 while (temp != NULL)
 
 {
-printf("[%d] %s\n", temp->len,
-temp->str != NULL ? temp->str : "(nil)");
+if (temp->str == NULL)
+printf("[0] (nil)\n");
 temp = temp->next;
-number_of_Node++;
+number_Of_Node++;
+}
+
+else
+{
+printf("[%u] %s\n", temp->len, temp->str);
+temp = temp->next;
+number_Of_Node++;
 }
 return (number_Of_Node);
 }
