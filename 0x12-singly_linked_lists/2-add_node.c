8099d30b90d85a43c24820c887ece2bf8be66116
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-07-29 19:45:17 +0100

    linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,31 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "lists.h"
+#include <stdlib.h>
 #include <string.h>
-
-/**
- * add_node - function that adds a new node at the beginning of a list_t list.
- *
- * @head: Header pointer
- * @str: String pointer
- *
- * Return: address of the new element, or NULL if it failed
- */
-
-list_t *add_node(list_t **head, const char *str)
-{
-	list_t *new_node;
-
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
-	
-	new_node->len = _strlen(str);
-	new_node->str = strdup(str);
-	new_node->next = *head;
-	*head = new_node;
-	return (new_node);
-}
 
 /**
  * _strlen - finds the length of a string
@@ -39,4 +14,32 @@ unsigned int _strlen(char *str)
 
 	for (i = 0; str[i]; i++);
 	return (i);
+}
+
+/**
+ * add_node - adds a node to the beginning of a linked list
+ * @head: double pointer to a lined list
+ * @str: string to add to the new node
+ *
+ * Return: pointer to the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+	return (new);
 }
