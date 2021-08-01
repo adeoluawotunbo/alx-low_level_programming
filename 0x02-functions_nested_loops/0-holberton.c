3804f035f9bc13a2a89566b97e882a27480853cb
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-01 15:15:07 +0100

    nested loops

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -4,21 +4,20 @@
 /**
  * main - the function prints Holberton.
  *
- * Return : Always 0 on success.
+ * Return: Always 0 on success.
  */
 
 int main(void)
 
 {
-_putchar('H');
-_putchar('o');
-_putchar('l');
-_putchar('b');
-_putchar('e');
-_putchar('r');
+_putchar('_');
+_putchar('p');
+_putchar('u');
 _putchar('t');
-_putchar('o');
-_putchar('n');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
