8b5958ce39845eb99bbcca5bb2414785935d8dee
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-01 15:26:57 +0100

    nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints  all alphabet in lowercase with new line.
+ *
+ * Return: Always 0 on sucess.
+ */
+
+void print_alphabet(void)
+{
+char alphabets;
+
+for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+}
