c4b0f8dc377558148f8c29a2caf269954160c141
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-01 17:56:06 +0100

    nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - 10 times alphabet, in lowercase
+ *
+ * Return: Always 0 on success
+ */
+
+void print_alphabet_x10(void)
+{
+int count;
+char alphabets;
+
+for (count = 0; count <= 9; count++)
+{
+for (alphabets = 97; alphabets <= 122; alphabets++)
+{
+_putchar(alphabets);
+}
+_putchar('\n');
+}
+}
