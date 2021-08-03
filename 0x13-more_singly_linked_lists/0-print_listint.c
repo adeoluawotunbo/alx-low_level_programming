f0522a957e5340557dea75aa1df5117637990ecb
ADEOLU AWOTUNBO <adeoluawotunbo@gmail.com>
2021-08-03 10:36:12 -0700

    more singly

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,12 @@
 #include "lists.h"
 
 /**
- * print_listint - a function that prints all the elements of a listint_t list.
+ * print_listint - a function that prints all the elements of
+ * a listint_t list.
  *
  * @h: pointer to the listint_t
+ * Description: a function thTAT PRINTS ALL
+ * THE ELEMENTS OF A LIST INT_LIST.
  * Return: the number of nodes
  */
 
@@ -11,7 +14,6 @@ size_t print_listint(const listint_t *h)
 
 {
 int nodes;
-
 while (h)
 {
 printf("%i\n", h->n);
