e581b1ac805a2ae16fd03c1937212528b191a40e
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-03 21:49:37 +0100

    more singly

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,7 +9,7 @@
  *
  * Description: a function that adds a new node at the beginning of
  * a listint_t list.
- * Return: address of the new element
+ * Return: address of the new element. 
  */
 
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -17,12 +17,14 @@ listint_t *add_nodeint(listint_t **head, const int n)
 listint_t *new_node;
 
 new_node = malloc(sizeof(listint_t));
+
 if (new_node == NULL)
 {
 return (NULL);
 }
+
 new_node->n = n;
 new_node->next = *head;
 *head = new_node;
-return (new_node);
+return (*head);
 }
