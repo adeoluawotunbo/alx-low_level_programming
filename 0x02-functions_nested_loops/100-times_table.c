559b0d73ec3d4daf335f32a743adf3da80ae4f43
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-17 13:24:57 +0100

    functions_nested_loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,34 +18,40 @@ void print_times_table(int n)
 
 	while (x <= n)
 	{
-		for (y= 0; y <= n; y++)
+		for (y = 0; y <= n; y++)
 		{
 			result = x * y;
-			if (result <= 9)
+			while (y = 0)
 			{
-				_putchar(' ');
-				_putchar(' ');
 				_putchar(result + '0');
 			}
-			else if ((result >= 10) && (result <= 99))
 			{
-				_putchar(' ');
-				_putchar((result / 10) + '0');
-				_putchar((result % 10) + '0');
+				if (result <= 9)
+				{	
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				else if ((result >= 10) && (result <= 99))
+				{
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
+				if (y != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
 			}
-			else
-			{
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
-			}
-			if (y != n)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
+			x++;
 		}
-		_putchar('\n');
-		x++;
 	}
 }
