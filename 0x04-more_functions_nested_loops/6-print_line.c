90641d434a84fc151a082e784c851638ea6374ce
adeoluawotunbo <adeoluawotunbo@gmail.com>
2021-08-21 17:28:17 +0100

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
 
 /**
- * print line - print lines
+ * print line - a function that draws a straight line in the terminal.
  * @n: first parameter
- * 
- * Description: '_' is input dependent
+ *
  * Return: Returns nothing since function is void
  */
 
 void print_line(int n)
 {
-int iteration;
-{
-for (iteration = 1; iteration <= n; iteration++)
-{
-_putchar('_');
-}
-_putchar('\n');
-}
+	int c;
+
+	count = 0;
+	while (count < n)
+	{
+		_putchar('_');
+
+		count++;
+	}
+	_putchar('\n');
 }
